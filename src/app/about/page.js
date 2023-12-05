25aadd54e750b09d6c54458cd769f744fcf236c8
Chris Kavanagh <ckava3@gmail.com>
2023-12-05 01:11:20 -0500

    navbar css

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div>
       <h1 className={styles.about}>About Page</h1>
-      <Link href="/">
+      <Link href="/" className={styles.aboutlink}>
         <h3>Home</h3>
       </Link>
     </div>
---
+++ src/app/components/topnav.js
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import styles from "../page.module.css";
+import { GiTowTruck } from "react-icons/gi";
+
+export default function Navbar() {
+  return (
+    <section className={styles.navbar}>
+      <nav className={styles.container}>
+        <div className={styles.logo}>
+          <GiTowTruck color="red" size="4em" />
+        </div>
+        <div className="nav-elements">
+          <ul className={styles.mylist}>
+            <li className={styles.navitems}>
+              <Link href="/">Home</Link>
+            </li>
+            <li className={styles.navitems}>
+              <Link href="/blog">Blog</Link>
+            </li>
+            <li className={styles.navitems}>
+              <Link href="/projects">Projects</Link>
+            </li>
+            <li className={styles.navitems}>
+              <Link href="/about">About</Link>
+            </li>
+            <li className={styles.navitems}>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </section>
+  );
+}
